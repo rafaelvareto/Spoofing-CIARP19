9e974846761085e549ab8640011e0e37f1adfbda
Rafael Vareto <rafaelvareto@gmail.com>
2018-09-15 13:28:25 -0300

    Fixing network error

--- deep_learning.py
+++ deep_learning.py
@@ -1,10 +1,4 @@
-from keras import preprocessing
-from keras.layers import Conv2D
-from keras.layers import Input
-from keras.layers import MaxPool2D
-from keras.layers import Activation
-from keras.layers import Flatten
-from keras.layers import Dense
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
 from keras.models import Sequential
 from keras.optimizers import SGD
 
@@ -14,27 +8,22 @@ class DeepLearning:
     def build_LeNet(width, height, depth, nclasses, weightsPath=None):
         # initialize the model
         model = Sequential()
-        input_shape = Input(shape=[width,height,depth])
-        print([width,height,depth])
+        print((width, height, depth))
 
         # first set of CONV => RELU => POOL
-        model.add(Conv2D(20, (5, 5), border_mode='same', name='conv1')(input_shape))
-        model.add(Activation('relu'))
-        model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(20, kernel_size=(5, 5), padding="same", activation='relu', input_shape=(width, height, depth)))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
         # second set of CONV => RELU => POOL
-        model.add(Conv2D(50, (5, 5), border_mode='same', name='conv2'))
-        model.add(Activation('relu'))
-        model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(50, kernel_size=(5, 5), activation='relu', padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
         # set of FC => RELU layers
         model.add(Flatten())
-        model.add(Dense(500))
-        model.add(Activation('relu'))
+        model.add(Dense(500, activation='relu'))
 
         # softmax classifier
-        model.add(Dense(nclasses))
-        model.add(Activation('softmax'))
+        model.add(Dense(nclasses, activation='softmax'))
 
         # if weightsPath is specified load the weights
         if weightsPath is not None:
