e4d0bad1eb0690fd4a27d13a54247edc2718b053
Rafael Vareto <rafaelvareto@gmail.com>
2018-09-18 14:39:43 -0300

    Regular commit

--- demo.py
+++ demo.py
@@ -71,7 +71,7 @@ def main():
 
         # Instantiate SpoofDet class
         spoofDet = FaceSpoofing()
-        spoofDet.obtain_video_features(folder_path=FOLDER_PATH, dataset_tuple=train_set, frame_drop=10, verbose=True)
+        spoofDet.obtain_video_features(folder_path=FOLDER_PATH, dataset_tuple=train_set, frame_drop=10, new_size=(400,300), verbose=True)
         spoofDet.trainPLS(components=10, iterations=1000) 
         # spoofDet.trainSVM(kernel_type='linear', verbose=False)
 
