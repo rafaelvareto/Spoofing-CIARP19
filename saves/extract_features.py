a099bde42daa4609ac68fd7b5dad7c7fd290b4c3
Rafael Vareto <rafaelvareto@gmail.com>
2018-09-19 11:41:50 -0300

    Implemention feature extraction file

---
+++ saves/extract_features.py
@@ -0,0 +1,100 @@
+import argparse
+import cv2 as cv
+import json
+import numpy as np
+import os
+import random
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+sys.path.append("..")
+from descriptors import Descriptors
+
+HOME = os.path.expanduser("~")
+
+def load_txt_file(file_name):
+    this_file = open(file_name, 'r')
+    this_list = list()
+    for line in this_file:
+        line = line.rstrip()
+        components = line.split()
+        this_list.append(components)
+    return this_list
+
+def get_fourier_spectrum(noise_img):
+    fft_img = np.fft.fft2(noise_img)
+    fts_img = np.fft.fftshift(fft_img)
+    mag_img = np.abs(fts_img)
+    log_img = np.log(1 + mag_img)
+    return log_img
+
+def get_residual_noise(gray_img, filter_type='median', kernel_size=7, gaussian_var=2.0):
+    if filter_type == 'median':
+        blurred = cv.medianBlur(src=gray_img, ksize=kernel_size)
+        noise = cv.subtract(gray_img, blurred)
+    elif filter_type == 'gaussian':
+        blurred = cv.GaussianBlur(src=gray_img, ksize=(kernel_size,kernel_size), sigmaX=gaussian_var, sigmaY=0.0)
+        noise = cv.subtract(gray_img, blurred)
+    else:
+        raise ValueError('ERROR: Two smoothing methods available: median and gaussian')
+    return noise
+
+def obtain_video_features(folder_path, dataset_tuple, frame_drop=1, size=(400,300), file_name='video_features.npy', verbose=False):
+    descriptor = Descriptors()
+    feature_list = list()
+    label_list = list()
+    path_list = list()
+
+    video_counter = 0
+    for (path, label) in dataset_tuple:
+        if verbose:
+            print(video_counter + 1, path, label)
+        frame_counter = 0
+        read_path = os.path.join(folder_path, path)
+        read_video = cv.VideoCapture(read_path)
+        while(read_video.isOpened()):
+            ret, read_frame = read_video.read()
+            if ret:
+                if frame_counter % frame_drop == 0:
+                    read_color = cv.resize(read_frame, (size[0], size[1]), interpolation=cv.INTER_AREA)
+                    read_greyd = cv.cvtColor(read_color, cv.COLOR_BGR2GRAY)
+                    read_noise = get_residual_noise(read_greyd, filter_type='median')
+                    read_spect = get_fourier_spectrum(noise_img=read_noise)
+                    read_featA = descriptor.get_hog_feature(image=read_greyd, pixel4cell=(64,64), cell4block=(1,1), orientation=8)
+                    read_featB = descriptor.get_glcm_feature(image=read_spect, dists=[1,2], shades=20)
+                    read_feats = np.concatenate((read_featA, read_featB), axis=0)
+                    if not np.any(np.isnan(read_feats)):
+                        feature_list.append(read_feats)
+                        label_list.append(label)
+                        path_list.append(path)
+            else:
+                break
+            frame_counter += 1
+        video_counter += 1
+        np.save(file_name, [feature_list, label_list, path_list])
+
+def main():
+    # Handle arguments
+    parser = argparse.ArgumentParser(description='Extracting Features from Dataset')
+    parser.add_argument('-f', '--folder_path', help='Path to video folder', required=False, default=os.path.join(HOME, "REMOTE/VMAIS/dataset/SiW_release"), type=str)
+    parser.add_argument('-te', '--testing_file', help='Path to testing txt file', required=False, default=os.path.join(HOME, "REMOTE/VMAIS/dataset/SiW_release/test_videos.txt"), type=str)
+    parser.add_argument('-tr', '--training_file', help='Path to training txt file', required=False, default=os.path.join(HOME, "REMOTE/VMAIS/dataset/SiW_release/train_videos.txt"), type=str)
+
+    # Storing in variables
+    args = parser.parse_args()
+    FOLDER_PATH = str(args.folder_path)
+    TEST_FILE = str(args.testing_file)
+    TRAIN_FILE = str(args.training_file)
+
+    # Split dataset into train and test sets
+    test_set = load_txt_file(file_name=TEST_FILE)
+    train_set = load_txt_file(file_name=TRAIN_FILE)
+
+    obtain_video_features(folder_path=FOLDER_PATH, dataset_tuple=train_set, frame_drop=1, size=(400,300), file_name='SiW-train.npy', verbose=True)
+    obtain_video_features(folder_path=FOLDER_PATH, dataset_tuple=test_set, frame_drop=1, size=(400,300), file_name='SiW-test.npy', verbose=True)
+
+if __name__ == "__main__":
+    main()
