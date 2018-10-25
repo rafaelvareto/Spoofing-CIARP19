47505f8f2960e13e3645d7294655bdd6898391ce
Rafael Vareto <rafaelvareto@gmail.com>
2018-10-25 13:18:34 -0300

    Adding script for protocol 03

---
+++ script_three.sh
@@ -0,0 +1,70 @@
+python3 demoFEAT.py -a 0.10 -b 050 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 075 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 125 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 150 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 200 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 300 -d 1 -i 100 -s three
+
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 200 -s three
+
+python3 demoFEAT.py -a 0.10 -b 050 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.10 -b 075 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.10 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.10 -b 125 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.10 -b 150 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.10 -b 200 -d 1 -i 200 -s three
+python3 demoFEAT.py -a 0.10 -b 300 -d 1 -i 200 -s three
+
+
+python3 demoFEAT.py -a 0.20 -b 050 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 075 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 125 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 150 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 200 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 300 -d 1 -i 100 -s three
+
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 200 -s three
+
+python3 demoFEAT.py -a 0.20 -b 050 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.20 -b 075 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.20 -b 125 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.20 -b 150 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.20 -b 200 -d 1 -i 200 -s three
+python3 demoFEAT.py -a 0.20 -b 300 -d 1 -i 200 -s three
+
+
+python3 demoFEAT.py -a 0.30 -b 050 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 075 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 125 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 150 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 200 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 300 -d 1 -i 100 -s three
+
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 200 -s three
+
+python3 demoFEAT.py -a 0.30 -b 050 -d 1 -i 050 -s three
+python3 demoFEAT.py -a 0.30 -b 075 -d 1 -i 075 -s three
+python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 100 -s three
+python3 demoFEAT.py -a 0.30 -b 125 -d 1 -i 125 -s three
+python3 demoFEAT.py -a 0.30 -b 150 -d 1 -i 150 -s three
+python3 demoFEAT.py -a 0.30 -b 200 -d 1 -i 200 -s three
+python3 demoFEAT.py -a 0.30 -b 300 -d 1 -i 200 -s three
--- script_two.sh
+++ script_two.sh
@@ -22,6 +22,30 @@ python3 demoFEAT.py -a 0.10 -b 200 -d 1 -i 200 -s two
 python3 demoFEAT.py -a 0.10 -b 300 -d 1 -i 200 -s two
 
 
+python3 demoFEAT.py -a 0.20 -b 050 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 075 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 125 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 150 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 200 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 300 -d 1 -i 100 -s two
+
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 050 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 075 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 125 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 150 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 200 -s two
+
+python3 demoFEAT.py -a 0.20 -b 050 -d 1 -i 050 -s two
+python3 demoFEAT.py -a 0.20 -b 075 -d 1 -i 075 -s two
+python3 demoFEAT.py -a 0.20 -b 100 -d 1 -i 100 -s two
+python3 demoFEAT.py -a 0.20 -b 125 -d 1 -i 125 -s two
+python3 demoFEAT.py -a 0.20 -b 150 -d 1 -i 150 -s two
+python3 demoFEAT.py -a 0.20 -b 200 -d 1 -i 200 -s two
+python3 demoFEAT.py -a 0.20 -b 300 -d 1 -i 200 -s two
+
+
 python3 demoFEAT.py -a 0.30 -b 050 -d 1 -i 100 -s two
 python3 demoFEAT.py -a 0.30 -b 075 -d 1 -i 100 -s two
 python3 demoFEAT.py -a 0.30 -b 100 -d 1 -i 100 -s two
