5763aa28ad279359f5e6766c18ad3aa8ac7929fc
Rafael Vareto <rafaelvareto@gmail.com>
2019-05-16 15:38:20 -0300

    Regular commit

--- saves/ERROR/hter_mean.py
+++ saves/ERROR/hter_mean.py
@@ -5,6 +5,7 @@ import os
 
 path = '.'
 json_files = [file for _root, _path, _file in os.walk(path) for file in _file if file.endswith('.json')]
+print(json_files)
 
 json_files.sort()
 for file in json_files:
@@ -12,4 +13,6 @@ for file in json_files:
 		json_data = json.load(json_ptr)
 		live_mean = np.mean(json_data['live'])
 		spoof_mean = np.mean(json_data['spoof'])
-		print('{:40.40}: LE {:.2f} | SE {:.2f} | MEAN {:.2f}'.format(file, live_mean, spoof_mean, (live_mean+spoof_mean)/2))
+		live_std = np.std(json_data['live'])
+		spoof_std = np.std(json_data['spoof'])
+		print('{:40.40}: LE {:.3f} | SE {:.3f} | MEAN {:.2f} | STD {:.2f}'.format(file, live_mean, spoof_mean, 100*(live_mean+spoof_mean)/2, 100*(live_std+spoof_std)/2))
